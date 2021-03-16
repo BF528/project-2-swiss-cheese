c475351aeeb8910edcc31acc05a0a7c6efe6107e
Neha Gupta <neha16@bu.edu>
2021-03-17 02:02:10 +0530

    Add files via upload

---
+++ Analyst.R
@@ -0,0 +1,47 @@
+#set working dir & reading table
+setwd("C:/Users/Neha/Desktop/BU/bf528/project2")
+data <- read.table('gene_exp.diff', header = TRUE)
+
+#ascending order based on qvalue
+order.value <- data[order(data$q_value),  ]
+order.value
+View(data)
+dim(data) #36329
+
+#top 10
+top_10 <- head(order.value[ , c(2, 3, 8, 9, 10, 12, 13)], 10)
+View(top_10)
+
+#histogram for all DEG
+graph = hist(data$log2.fold_change., breaks = 40, main ='Based on log2 fold change values', col = 'purple')
+
+#subsetting significant genes
+significant.df <- subset(x = data, significant == 'yes' )
+dim(significant.df) #5193
+
+View(significant.df)
+
+#What do you notice?
+#Ans-  significant genes has logfold values in negative as well
+#histogram for significant DEG
+sign.graph <- hist(significant.df$log2.fold_change., breaks = 40, main = 'Log 2 fold of significantly expressed genes ', col = 'orange')
+sign_p_value <- subset(data, data$p_value < 0.01)#4,686
+sign_q_value <- subset(data, data$q_value < 0.01) #3,788 
+View(sign_p_value_up)
+
+
+up_sign_pvalue <- subset(sign_p_value, sign_p_value$log2.fold_change. >0 )
+down_sign_pvalue <- subset(sign_p_value, sign_p_value$log2.fold_change. < 0 )
+View(down_sign_pvalue)
+
+#separating up and down regualted genes
+up <- subset(significant.df, significant.df$log2.fold_change. > 0) #2760
+down <- subset(significant.df, significant.df$log2.fold_change. <0) #2433
+cat("The number of up-regulated significant genes: ", dim(up)[1])
+cat("The number of down-regulated significant genes: ", dim(down)[1])
+
+#creating csv files
+write(x = up$gene, file = 'up_log2fold_change_genes.csv')
+write(x = down$gene, file = 'down_log2fold_change.csv')
+
+
